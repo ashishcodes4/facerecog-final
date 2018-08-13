b48befef482dda8d123d42a3c82a2080e7308490
Ashish Singh <ashishcodes4@gmail.com>
2018-08-13 19:49:42 +0530

    initial commit

---
+++ facerecog-final/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TopBar from './Components/TopBar';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TopBar />
+      </div>
+    );
+  }
+}
+
+export default App;
